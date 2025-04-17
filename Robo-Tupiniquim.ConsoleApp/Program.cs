8dd824c3e87781a36c1c28edd27333849cbbc0aa
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-04-17 13:35:38 -0300

    Add project files.

---
+++ Robo-Tupiniquim.ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Robo_Tupiniquim.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int posicaoX = 0;
+            int posicaoY = 0;
+            string localizacaoRobo = $"{posicaoX},{posicaoY}";
+            string direcaoRobo = "N"; //Direção começa
+            string instrucao = "DDD".ToUpper();
+
+            for (int i = 0; i < instrucao.Length; i++)
+            {
+                char comando = instrucao[i];
+
+                if (comando == 'D')
+                {
+                    if (direcaoRobo == "N")
+                    {
+                        direcaoRobo = "L";
+                    }
+                    else if (direcaoRobo == "L")
+                    {
+                        direcaoRobo = "S";
+                    }
+                    else if (direcaoRobo == "S")
+                    {
+                        direcaoRobo = "O";
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Comando inválido: Utilize [D], para direita, ou [E] para esquerda!");
+                    //Console.ReadLine();
+                    break;
+                }
+
+            }
+            Console.WriteLine($"\nLocalização do robô:{localizacaoRobo},{direcaoRobo}");
+            Console.ReadLine();
+        }
+    }
+}
